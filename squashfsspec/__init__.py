3fd26f9be09239eaea0cb2054aaf0ac2ca9d8e86
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2026-09-15 13:28:40 +0200

    Add author, project URLs, keywords and __version__ (#32)

--- squashfsspec/__init__.py
+++ squashfsspec/__init__.py
@@ -1,4 +1,12 @@
+# Standard library
+from importlib.metadata import PackageNotFoundError, version
+
 # First-party
 from squashfsspec.squashfsspec import OffsetWrapper, SquashFSFileSystem
 
-__all__ = ["SquashFSFileSystem", "OffsetWrapper"]
+try:
+    __version__ = version("squashfsspec")
+except PackageNotFoundError:  # pragma: no cover - source tree without install
+    __version__ = "0+unknown"
+
+__all__ = ["SquashFSFileSystem", "OffsetWrapper", "__version__"]
